8c8f1bd9b1d166889e9688f399ba467e390c1f5e
niharika <niharikaiec@gmail.com>
2020-02-22 17:57:01 -0800

    working on build script

---
+++ testtollapplication.sh
@@ -0,0 +1,6 @@
+curl -X GET \
+  http://localhost:9082/api/v1/datasource/1005 \
+  -H 'Authorization: Basic ZmlnaHRjbHViOmlhbWlu' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 29457052-bfa6-4d15-adfe-8bfcaa5d25ec'
